c704cff033c2f9b832dd21a299582dd75ee892b2
Lokesh071 <lokeshgudimetla121@gmail.com>
2025-05-03 13:22:28 +0530

    Initial commit

---
+++ run_nature.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Activating virtual environment...
+call tf-env\Scripts\activate.bat
+
+if %errorlevel% neq 0 (
+    echo Error activating virtual environment.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Setting up database...
+mysql -u root -p"pass11" -P 3307 < setup_database.sql
+
+if %errorlevel% neq 0 (
+    echo Error setting up database. Please check MySQL connection and credentials.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Database setup successful.
+echo Starting Flask server...
+python app.py
+
+pause 
